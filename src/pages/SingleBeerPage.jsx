83be5af246b1d0dffe646c9157bfde1c53fffebf
alex-dev-cloud <alex.dev@gmail.com>
2021-12-02 22:51:08 +0200

    add styling changes

--- src/pages/SingleBeerPage.jsx
+++ src/pages/SingleBeerPage.jsx
@@ -24,21 +24,21 @@ const SingleBeerPage = observer(() => {
     return (
 
         <div>
-            <div className="container d-flex f-center">
+            <div className="container d-flex f-center beer-container">
                 <div className="imageContainer">
                     <img src={beer.image_url ?? fallBackImg} alt={beer.name}/>
                 </div>
                 <div className="infoContainer">
-                    <h2>Alcohol By Volume: {beer.abv}%</h2>
-                    <p className="tagline">{beer.tagline}</p>
+                    <h2 className="tagline">" {beer.tagline} "</h2>
+                    <h3 className="abv">Alcohol By Volume <span>{beer.abv}%</span></h3>
                     <div className="description">
                         <ShowMoreText width={900}>{beer.description}</ShowMoreText>
                     </div>
                     <h3>This beer pair with:</h3>
-                    <ol>
+                    <ol className="pair-list">
                         {
                             beer.food_pairing?.map((food, index) =>
-                                <li key={index}>{food}</li>
+                                <li key={index}>{`${index + 1}. ${food}`}</li>
                             )
                         }
                     </ol>
