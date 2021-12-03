4971df32819a90912c2ba026b1125d2ae33b7814
alex-dev-cloud <alex.dev@gmail.com>
2021-12-03 15:27:19 +0200

    add react icons

--- src/components/SortingBar/SortingBar.jsx
+++ src/components/SortingBar/SortingBar.jsx
@@ -1,12 +1,13 @@
 import React, {useContext, useState} from 'react';
 import classes from './SortingBar.module.scss';
 import {Context} from '../../index';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSort, faSortDown, faSortUp} from '@fortawesome/free-solid-svg-icons';
+import {BiSort, BiSortDown, BiSortUp} from "react-icons/bi";
 
 const SortingBar = () => {
 
     const {beerStore} = useContext(Context);
+
     const [sortingType, setSortingType] = useState(null);
     const [sortDirection, setSortDirection] = useState('asc');
 
@@ -28,22 +29,22 @@ const SortingBar = () => {
         beerStore.beers.sort(sorting);
     };
 
-    const getSortIconClass = sorting => {
+    const getSortIcon = sorting => {
         if (sorting !== sortingType) {
-            return faSort;
+            return <BiSort/>;
         }
-        return sortDirection === 'asc' ? faSortUp : faSortDown;
+        return sortDirection === 'asc' ? <BiSortUp/> :  <BiSortDown/>;
     };
 
     return (
         <div className={classes.container}>
             <div onClick={sortingByName} className={sortingType === 'name' ? `${classes.item} + ${classes.active}` : classes.item}>
                 <span className="btn-title">By name</span>
-                <FontAwesomeIcon icon={getSortIconClass('name')}/>
+                {getSortIcon('name')}
             </div>
             <div onClick={sortingByAbv} className={sortingType === 'abv' ? `${classes.item} + ${classes.active}` : classes.item}>
                 <span className="btn-title">By alcohol</span>
-                <FontAwesomeIcon icon={getSortIconClass('abv')}/>
+                {getSortIcon('abv')}
             </div>
         </div>
     );
